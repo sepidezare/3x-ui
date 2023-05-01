b2ea1387e8c0168a0a209dab689aea04d77de667
sepidezare <52865103+sepidezare@users.noreply.github.com>
2023-05-01 21:10:33 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -77,95 +77,62 @@ config_after_install() {
         read -p "Please set up your username:" config_account
         echo -e "${yellow}Your username will be:${config_account}${plain}"
         read -p "Please set up your password:" config_password
-        echo -e "${yellow}Your password will be:${config_password}${plain}"
-        read -p "Please set up the panel port:" config_port
-        echo -e "${yellow}Your panel port is:${config_port}${plain}"
-        echo -e "${yellow}Initializing, please wait...${plain}"
-        /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
-        echo -e "${yellow}Account name and password set successfully!${plain}"
-        /usr/local/x-ui/x-ui setting -port ${config_port}
-        echo -e "${yellow}Panel port set successfully!${plain}"
-    else
-        echo -e "${red}cancel...${plain}"
-        if [[ ! -f "/etc/x-ui/x-ui.db" ]]; then
-            local usernameTemp=$(head -c 6 /dev/urandom | base64)
-            local passwordTemp=$(head -c 6 /dev/urandom | base64)
-            /usr/local/x-ui/x-ui setting -username ${usernameTemp} -password ${passwordTemp}
-            echo -e "this is a fresh installation,will generate random login info for security concerns:"
-            echo -e "###############################################"
-            echo -e "${green}username:${usernameTemp}${plain}"
-            echo -e "${green}password:${passwordTemp}${plain}"
-            echo -e "###############################################"
-            echo -e "${red}if you forgot your login info,you can type x-ui and then type 7 to check after installation${plain}"
-        else
-            echo -e "${red} this is your upgrade,will keep old settings,if you forgot your login info,you can type x-ui and then type 7 to check${plain}"
-        fi
-    fi
-}
-
+        echo -e "${yellow}Your password will
 install_x-ui() {
-    systemctl stop x-ui
-    cd /usr/local/
+systemctl stop x-ui
+cd /usr/local/
+if [ $# == 0 ]; then
+    last_version=$(curl -Ls "https://api.github.com/repos/sepidezare/3x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/."([^"]+)"./\1/')
+else
+    last_version=$1
+fi
 
-  
-        fi
-        echo -e "Got x-ui latest version: ${last_version}, beginning the installation..."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz https://github.com/sepidezare/3x-ui/x-ui-linux-$(arch3xui).tar.gz
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}Downloading x-ui failed, please be sure that your server can access Github ${plain}"
-            exit 1
-        fi
-    else
-        last_version=$1
-        url="https://github.com/sepidezare/3x-ui/x-ui-linux-$(arch3xui).tar.gz"
-        echo -e "Begining to install x-ui $1"
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz ${url}
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}Download x-ui $1 failed,please check the version exists ${plain}"
-            exit 1
-        fi
+echo -e "${green}start to install x-ui ${last_version}${plain}"
+
+if [[ -d "/usr/local/x-ui" ]]; then
+    if [[ -d "/usr/local/x-ui.bak" ]]; then
+        rm -rf /usr/local/x-ui.bak
     fi
+    mv /usr/local/x-ui /usr/local/x-ui.bak
+    echo -e "${yellow}Backup the old version to /usr/local/x-ui.bak${plain}"
+fi
 
-    if [[ -e /usr/local/x-ui/ ]]; then
-        rm /usr/local/x-ui/ -rf
-    fi
+if [[ ${last_version} == "install" ]];then
+    wget -N --no-check-certificate -O /tmp/x-ui.zip https://github.com/sepidezare/3x-ui/archive/main.zip
+else
+    wget -N --no-check-certificate -O /tmp/x-ui.tar.gz https://github.com/sepidezare/3x-ui/archive/${last_version}.tar.gz
+fi
 
-    tar zxvf x-ui-linux-$(arch3xui).tar.gz
-    rm x-ui-linux-$(arch3xui).tar.gz -f
-    cd x-ui
-    chmod +x x-ui bin/xray-linux-$(arch3xui)
-    cp -f x-ui.service /etc/systemd/system/
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/sepidezare/3x-ui/main/x-ui.sh
-    chmod +x /usr/local/x-ui/x-ui.sh
-    chmod +x /usr/bin/x-ui
-    config_after_install
-    #echo -e "If it is a new installation, the default web port is ${green}2053${plain}, The username and password are ${green}admin${plain} by default"
-    #echo -e "Please make sure that this port is not occupied by other procedures,${yellow} And make sure that port 2053 has been released${plain}"
-    #    echo -e "If you want to modify the 2053 to other ports and enter the x-ui command to modify it, you must also ensure that the port you modify is also released"
-    #echo -e ""
-    #echo -e "If it is updated panel, access the panel in your previous way"
-    #echo -e ""
-    systemctl daemon-reload
-    systemctl enable x-ui
+if [[ $? -ne 0 ]]; then
+    echo -e "${red}Failed to download the x-ui file, please check your network${plain}"
+    exit 1
+fi
+
+tar zxf /tmp/x-ui.tar.gz -C /usr/local/
+mv /usr/local/3x-ui* /usr/local/x-ui
+chmod +x /usr/local/x-ui/x-ui.sh
+
+if [[ ! -f "/usr/local/x-ui/app/app.db" ]]; then
+    echo -e "${green}Starting up the x-ui app for the first time${plain}"
+    /usr/local/x-ui/x-ui.sh install
+else
+    echo -e "${green}Starting up the x-ui app${plain}"
     systemctl start x-ui
-    echo -e "${green}x-ui ${last_version}${plain} installation finished, it is running now..."
-    echo -e ""
-    echo -e "x-ui control menu usages: "
-    echo -e "----------------------------------------------"
-    echo -e "x-ui              - Enter     Admin menu"
-    echo -e "x-ui start        - Start     x-ui"
-    echo -e "x-ui stop         - Stop      x-ui"
-    echo -e "x-ui restart      - Restart   x-ui"
-    echo -e "x-ui status       - Show      x-ui status"
-    echo -e "x-ui enable       - Enable    x-ui on system startup"
-    echo -e "x-ui disable      - Disable   x-ui on system startup"
-    echo -e "x-ui log          - Check     x-ui logs"
-    echo -e "x-ui update       - Update    x-ui"
-    echo -e "x-ui install      - Install   x-ui"
-    echo -e "x-ui uninstall    - Uninstall x-ui"
-    echo -e "----------------------------------------------"
+fi
+
+sleep 3
+
+if [[ $(systemctl is-active x-ui) != "active" ]]; then
+    echo -e "${red}Failed to start x-ui service, please check the installation logs${plain}"
+    exit 1
+fi
+
+echo -e "${green}x-ui ${last_version} has been installed successfully!${plain}"
+config_after_install
 }
 
-echo -e "${green}Running...${plain}"
+arch3xui
 install_base
 install_x-ui $1
+
+exit 0
